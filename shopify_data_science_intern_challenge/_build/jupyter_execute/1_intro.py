201579b0b05965451c2a6e77810c01edc61de1d7
Macy Chan <macychan@student.ubc.ca>
2022-01-07 11:18:11 -0500

    Update wordings

--- shopify_data_science_intern_challenge/_build/jupyter_execute/1_intro.py
+++ shopify_data_science_intern_challenge/_build/jupyter_execute/1_intro.py
@@ -5,10 +5,9 @@
 # 
 # This notebook is created for Shoptify Data Science Intern Challenge (Summer 2022)
 # 
+# >For **details** of steps and EDA, click [Question1](https://macychan.github.io/shopify_data_science_intern_challenge/2_question1.html) [Question2](https://macychan.github.io/shopify_data_science_intern_challenge/3_question2.html)  
 # 
 # >For **summary** of answers, click [Here](https://macychan.github.io/shopify_data_science_intern_challenge/4_summary.html)  
-# 
-# >For **details** of steps and EDA, click [Question1](https://macychan.github.io/shopify_data_science_intern_challenge/2_question1.html) [Question2](https://macychan.github.io/shopify_data_science_intern_challenge/3_question2.html)  
 # 
 # Here is my personal [webpage](https://www.macy-chan.com)
 
